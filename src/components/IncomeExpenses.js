8bafcef2c8dc5177de68ef401ee35e5435405247
Dharmini <dharmini.S@Rams-MacBook-Pro.local>
2024-09-04 21:18:14 -0700

    Rendered salary/expense component and rendered gloabal context

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
 
 export const IncomeExpenses = () => {
+
+  const {transactions} = useContext(GlobalContext);
+  //map through each value of transaction. If it is +ve, it gets added
+  //Income. If -ve, it gets added to expense
+  let income;
+  let expense;
+  transactions.forEach((transaction)=>{
+    if (transaction.amount>0)
+      {
+        income+= transaction.amount
+      }
+    else if(transaction.amount<0)
+     {
+        expense-=transaction.amount
+     }
+  })
   return (
     <div>
       <div className="inc-exp-container">
         <div>
           <h4>Income</h4>
-          <p className="money plus">+$0.0</p>
+          <p className="money plus">${income}</p>
         </div>
         <div>
           <h4>Expense</h4>
-          <p className="money minus">-$0.0</p>
+          <p className="money minus">${expense}</p>
         </div>
       </div>
     </div>
